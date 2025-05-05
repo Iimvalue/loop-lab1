211da69fec8dbb10eee606816fc64f34b127ccc4
Abdulrahman <1xvlux1@gmail.com>
2025-05-05 10:15:26 +0300

    apps

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("<<<<< Even Numbers >>>>>");
+
+for (let i = -25; i < 1; i++) {
+  if (i % 2 == 0) console.log(i);
+}
+
+console.log("<<<<< Power Numbers >>>>>");
+
+for (let i = 1; i < 11; i++) {
+  console.log(`${i} x ${i} = ${i * i}`);
+}
+
+console.log("<<<<< Odd Numbers >>>>>");
+for (let i = 1; i < 21; i++) {
+  if (i % 2 != 0) console.log(i);
+}
+
+// Fibonacci sequence
+
+console.log("<<<<< Fibonacci sequence >>>>>");
+
+let number1 = 0;
+let number2 = 1;
+let temp;
+
+for (let i = 1; i < 11; i++) {
+  console.log(number1);
+  temp = number1 + number2;
+
+  number1 = number2;
+  number2 = temp;
+}
