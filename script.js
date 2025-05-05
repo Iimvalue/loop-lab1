38caa391c960088a21931485a51e2185fd84d0ea
Abdulrahman <1xvlux1@gmail.com>
2025-05-05 10:19:13 +0300

    mult

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
-console.log("<<<<< Even Numbers >>>>>");
+// console.log("<<<<< Even Numbers >>>>>");
 
-for (let i = -25; i < 1; i++) {
-  if (i % 2 == 0) console.log(i);
-}
+// for (let i = -25; i < 1; i++) {
+//   if (i % 2 == 0) console.log(i);
+// }
 
-console.log("<<<<< Power Numbers >>>>>");
+// console.log("<<<<< Power Numbers >>>>>");
 
-for (let i = 1; i < 11; i++) {
-  console.log(`${i} x ${i} = ${i * i}`);
-}
+// for (let i = 1; i < 11; i++) {
+//   console.log(`${i} x ${i} = ${i * i}`);
+// }
 
-console.log("<<<<< Odd Numbers >>>>>");
-for (let i = 1; i < 21; i++) {
-  if (i % 2 != 0) console.log(i);
-}
+// console.log("<<<<< Odd Numbers >>>>>");
+// for (let i = 1; i < 21; i++) {
+//   if (i % 2 != 0) console.log(i);
+// }
 
-// Fibonacci sequence
+// // Fibonacci sequence
 
-console.log("<<<<< Fibonacci sequence >>>>>");
+// console.log("<<<<< Fibonacci sequence >>>>>");
 
-let number1 = 0;
-let number2 = 1;
-let temp;
+// let number1 = 0;
+// let number2 = 1;
+// let temp;
 
-for (let i = 1; i < 11; i++) {
-  console.log(number1);
-  temp = number1 + number2;
+// for (let i = 1; i < 11; i++) {
+//   console.log(number1);
+//   temp = number1 + number2;
 
-  number1 = number2;
-  number2 = temp;
+//   number1 = number2;
+//   number2 = temp;
+// }
+
+
+// Multiplication table
+
+let sum = 0;
+for(let i = 1; i<4; i++){
+    for(let j =1; j<10; j++){
+        sum = i * j;
+        console.log(`${i} x ${j} = ${sum}`)
+    }
 }
